6e17c79f9a2fca6e0783b60d5f29984e094c81a6
vic123 <zhangzc_efz@163.com>
2024-05-27 22:27:04 +0800

    add src code and resources for tetris

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,9 @@
-#include <SFML/Graphics.hpp>
+#include "tetris.hpp"
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
-
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
-    }
+int main( int argc , char **argv ){
+  std::srand( std::time( 0 ) );
+  auto tetris = std::make_shared<Tetris>();
+  tetris->run();
+  return 0;
 }
+
