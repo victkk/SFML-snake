c6aa550807296bfb35b57bd6cdba51d763b64539
vic123 <zhangzc_efz@163.com>
2024-05-31 16:08:00 +0800

    interface class for screens

---
+++ src/screen/IScreen.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+enum class SCREEN
+{
+    GAME,
+    PAUSE
+};
+
+class IScreen
+{
+public:
+    virtual void handleInput(sf::RenderWindow &window) = 0;
+    virtual void update() = 0;
+    virtual void render(sf::RenderWindow &window) = 0;
+    virtual void run(sf::RenderWindow &window) = 0;
+    virtual SCREEN nextScreenLogic(sf::RenderWindow &window) = 0;
+};
