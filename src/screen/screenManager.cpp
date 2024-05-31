02fb886ae828c87535dee0879898240a1be0f2b1
vic123 <zhangzc_efz@163.com>
2024-05-31 16:08:26 +0800

    add screen manager

---
+++ src/screen/screenManager.cpp
@@ -0,0 +1,58 @@
+
+#include "screenManager.hpp"
+ScreenManager::ScreenManager() : pauseScreen(), gameScreen(400), window(sf::VideoMode(800, 600), "SFML")
+{
+    currentScreen = &gameScreen;
+}
+void ScreenManager::run()
+{
+    sf::Clock clock;
+    sf::Clock test_clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
+
+    // 主循环运行，直到窗口关闭
+    while (window.isOpen())
+    {
+        // 事件处理
+        sf::Time elapsedTime = clock.restart();
+        timeSinceLastUpdate += elapsedTime;
+        sf::Event event;
+
+        if (timeSinceLastUpdate > timePerFrame)
+        {
+
+            while (timeSinceLastUpdate > timePerFrame)
+                timeSinceLastUpdate -= timePerFrame;
+
+            currentScreen->run(window);
+            nextScreenLogic();
+            while (window.pollEvent(event))
+            {
+                switch (event.type)
+                {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+
+                default:
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void ScreenManager::nextScreenLogic()
+{
+    switch (currentScreen->nextScreenLogic(window))
+    {
+    case SCREEN::PAUSE:
+        currentScreen = &pauseScreen;
+        break;
+    case SCREEN::GAME:
+        currentScreen = &gameScreen;
+        break;
+    default:
+        break;
+    }
+}
---
+++ src/screen/screenManager.hpp
@@ -0,0 +1,18 @@
+#include "gameScreen.hpp"
+#include "pauseScreen.hpp"
+
+class ScreenManager
+{
+private:
+    sf::RenderWindow window;
+    IScreen *currentScreen;
+    PauseScreen pauseScreen;
+    GameScreen gameScreen;
+
+public:
+    ScreenManager();
+    void run();
+
+private:
+    void nextScreenLogic();
+};
